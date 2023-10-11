272ce035165cc9c906c169de676f62495d568f08
Jkhang2404 <khangdjnh1910@gmail.com>
2023-10-11 22:00:30 +0700

    Using Image

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.diceroller
 
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -19,13 +19,33 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice1 = Dice(6)
         val diceRoll1 = dice1.roll()
-        val resultTextView1: TextView = findViewById(R.id.textView1)
-        resultTextView1.text = diceRoll1.toString()
+        //val resultTextView1: TextView = findViewById(R.id.textView1)
+        //resultTextView1.text = diceRoll1.toString()
+        val resultImageView1: ImageView = findViewById(R.id.imageView1)
+        resultImageView1.setImageResource(R.drawable.dice1)
+
+        if(diceRoll1 == 1){ resultImageView1.setImageResource(R.drawable.dice1) }
+        if(diceRoll1 == 2){ resultImageView1.setImageResource(R.drawable.dice2) }
+        if(diceRoll1 == 3){ resultImageView1.setImageResource(R.drawable.dice3) }
+        if(diceRoll1 == 4){ resultImageView1.setImageResource(R.drawable.dice4) }
+        if(diceRoll1 == 5){ resultImageView1.setImageResource(R.drawable.dice5) }
+        if(diceRoll1 == 6){ resultImageView1.setImageResource(R.drawable.dice6) }
+
 
         val dice2 = Dice(6)
         val diceRoll2 = dice2.roll()
-        val resultTextView2: TextView = findViewById(R.id.textView2)
-        resultTextView2.text = diceRoll2.toString()
+        //val resultTextView2: TextView = findViewById(R.id.textView2)
+        //resultTextView2.text = diceRoll2.toString()
+        val resultImageView2: ImageView = findViewById(R.id.imageView2)
+        resultImageView2.setImageResource(R.drawable.dice1)
+
+        if(diceRoll2 == 1){ resultImageView2.setImageResource(R.drawable.dice1) }
+        if(diceRoll2 == 2){ resultImageView2.setImageResource(R.drawable.dice2) }
+        if(diceRoll2 == 3){ resultImageView2.setImageResource(R.drawable.dice3) }
+        if(diceRoll2 == 4){ resultImageView2.setImageResource(R.drawable.dice4) }
+        if(diceRoll2 == 5){ resultImageView2.setImageResource(R.drawable.dice5) }
+        if(diceRoll2 == 6){ resultImageView2.setImageResource(R.drawable.dice6) }
+
     }
 }
 
